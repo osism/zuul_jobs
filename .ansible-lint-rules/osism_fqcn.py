dc23bdcbdeed3a6a3479ade71ab2eb6e19b6f3f5
Christian Berendt <berendt@osism.tech>
2023-07-22 12:25:09 +0200

    Improve the osism-fqcn ansible-lint rule (#42)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- .ansible-lint-rules/osism_fqcn.py
+++ .ansible-lint-rules/osism_fqcn.py
@@ -1,8 +1,9 @@
 """Rule definition for usage of fully qualified collection names for FQCN."""
-import sys
-import yaml
+
 import os
+import sys
 from typing import Any, Dict, Optional, Union
+import yaml
 
 from ansiblelint.file_utils import Lintable
 from ansiblelint.rules import AnsibleLintRule
@@ -13,23 +14,27 @@ class OsismFQCNRule(AnsibleLintRule):
 
     id = "osism-fqcn"
     severity = "MEDIUM"
-    description = (
-        "Check whether the long version is used in the playbook"
-    )
+    description = "Check whether the long version is used in the playbook"
     tags = ["formatting"]
 
     def matchtask(
         self, task: Dict[str, Any], file: Optional[Lintable] = None
     ) -> Union[bool, str]:
 
-        with open(f"{os.getcwd()}/.ansible-lint-rules/osism_fqcn_list.yaml", 'r') as fileStream:
+        with open(
+            f"{os.getcwd()}/.ansible-lint-rules/osism_fqcn_list.yaml", "r"
+        ) as fp:
             try:
-                osism_fqcn_list = yaml.safe_load(fileStream)
-            except yaml.YAMLError as exception:
-                print(exception)
-                sys.exit(0)
+                data = yaml.safe_load(fp)
+            except yaml.YAMLError as e:
+                print(e)
+                sys.exit(1)
 
-        for category in osism_fqcn_list:
-            if task["action"]["__ansible_module_original__"] in osism_fqcn_list[category]:
+        for category in data:
+            if (
+                task["action"]["__ansible_module_original__"]
+                in data[category]
+            ):
                 return False
+
         return True
